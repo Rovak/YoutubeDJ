bed4cb1a2f6dc98c999ccf549b3da65abbf0fa86
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-02-15 20:02:17 +0100

    doc update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var   express     = require('express')
+var   express   = require('express')
     , http      = require('http')
     , server    = require('./server/server')
     , path      = require('path')
