a6401638cfc02ae25fdb9edd5504dff4fe265476
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-02-17 17:10:49 +0100

    Moved build tasks to grunt, fixes #3

--- grunt.js
+++ grunt.js
@@ -12,8 +12,62 @@ module.exports = function(grunt) {
         },
         lint: {
             src: ["public/js/**/*.js"]
+        },
+        jsduck: {
+            main: {
+                src: [
+                    'src/server',
+                    'src/shared',
+                    'src/client/controllers',
+                    'src/client/client.js',
+                    'src/client/public/mobile',
+                    'src/client/public/player'
+                ],
+                dest: 'build/api',
+                options: {
+                    'builtin-classes': true,
+                    'title': 'Youtube DJ'
+                }
+            }
+        },
+        clean: {
+            build: ["build"]
+        },
+        watch: {
+            files: ['sass/*.scss'],
+            tasks: ['compass:dev']
+        },
+        compass: {
+            dev: {
+                src: 'sass',
+                dest: 'src/client/public/css',
+                linecomments: true,
+                forcecompile: true,
+                require: [],
+                debugsass: true,
+                images: 'images',
+                relativeassets: true
+            },
+            prod: {
+                src: 'sass',
+                dest: 'src/client/public/css',
+                linecomments: false,
+                forcecompile: true,
+                require: [],
+                debugsass: false,
+                images: 'images',
+                relativeassets: true
+            }
         }
     });
 
-    grunt.registerTask('default', ['lint', 'jshint']);
+    grunt.registerTask('prepare', function() {
+        grunt.file.mkdir('build');
+    });
+
+    grunt.loadNpmTasks('grunt-jsduck');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-compass');
+
+    grunt.registerTask('default', ['clean', 'prepare', 'compass:prod', 'jsduck']);
 };
