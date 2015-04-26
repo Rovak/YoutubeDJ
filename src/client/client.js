d349334b62c5df9675ae6c1d74d1bb381ffa984f
Roy van Kaathoven <ik@royvankaathoven.nl>
2015-04-26 14:02:55 +0200

    updated to latest express.js

--- src/client/client.js
+++ src/client/client.js
@@ -1,26 +1,28 @@
-var express = require('express')
-        , http = require('http')
-        , path = require('path')
-        , config = require('nconf')
-        , fs = require('fs');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var http = require('http')
+var path = require('path')
+var config = require('nconf')
+var fs = require('fs');
 
 exports.createServer = function() {
+
     var app = express();
 
-    app.configure(function() {
-        app.set('port', config.get("webserver:port"));
-        app.set('views', __dirname + '/views');
-        app.set('view engine', 'ejs');
-        app.use(express.logger('dev'));
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(app.router);
-        app.use(express.static(__dirname + '/public'));
-    });
+    var port = config.get("webserver:port");
+    app.set('port', port);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
 
-    app.configure('development', function() {
-        app.use(express.errorHandler());
-    });
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
     // Initialize controllers
     var controllersFolder = __dirname + "/controllers/";
@@ -31,10 +33,37 @@ exports.createServer = function() {
         require(controllerPath)(app);
     }
 
-    // Initialize socket.io server
-    var httpServer = http.createServer(app);
-
-    httpServer.listen(app.get('port'), function() {
-        console.log("DJ Server active on port" + app.get('port'));
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
     });
+
+    // error handlers
+
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+      app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+          message: err.message,
+          error: err
+        });
+      });
+    }
+
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: {}
+      });
+    });
+
+    var server = http.createServer(app);
+    server.listen(port);
 };
